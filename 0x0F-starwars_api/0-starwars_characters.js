5f97640857a62c5327e43f12efd00dd7a00ea646
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-06-05 00:31:28 +0200

    Update

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,31 +1,26 @@
 #!/usr/bin/node
-
-// Write a script that prints all characters of a Star Wars movie:
-// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
-// Display one character name per line in the same order as the “characters” list in the /films/ endpoint
-// You must use the Star wars API
-// You must use the request module
-
 const request = require('request');
 
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
+
+request(url, async (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
+    for (const characterUrl of characters) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
           if (error) {
-            reject(error);
+            console.error(error);
           } else {
-            resolve(JSON.parse(html).name);
+            const character = JSON.parse(body);
+            console.log(character.name);
           }
+          resolve();
         });
       });
-      console.log(res);
     }
   }
 });

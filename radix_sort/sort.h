70a2ab2cf592e0dc673de0281bfd4ae4d3cc3cea
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-10-10 07:32:43 +0200

    function that sorts an array of integers in ascending order using the Radix sort algorithm

---
+++ radix_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+int find_max(int *arr, int n);
+void countingSort(int *array, int n, int pos);
+void lsdRadixSort(int arr[], int arr1[], int len);
+
+#endif

eef752a0308db145e4de76b36e28ee5f1c838e24
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-08 22:04:01 +0100

    Update sort.h

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -2,8 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
-
-void merge_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-
+void merge(int *array, int *buffer, size_t left, size_t mid, size_t right)
+void merge_sort_recursive(int *array, int *buffer, size_t left, size_t right)
+void merge_sort(int *array, size_t size)
 #endif

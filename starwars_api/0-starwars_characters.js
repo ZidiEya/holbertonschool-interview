c44eca62f441a7d395c396ad89e34c3abfd7cc54
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-09 20:19:45 +0100

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,31 +2,35 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+if (process.argv.length < 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie_ID>');
   process.exit(1);
 }
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (error) return;
+request(apiUrl, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-  const film = JSON.parse(body);
-  const characters = film.characters;
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
 
-  printCharacters(characters, 0);
+  // Fetch character names in order, one by one (sequentially)
+  const fetchCharacter = (index) => {
+    if (index >= characters.length) return;
+
+    request(characters[index], (err, res, body) => {
+      if (!err) {
+        const character = JSON.parse(body);
+        console.log(character.name);
+        fetchCharacter(index + 1);
+      }
+    });
+  };
+
+  fetchCharacter(0);
 });
-
-function printCharacters (characters, index) {
-  if (index >= characters.length) return;
-
-  request(characters[index], (error, response, body) => {
-    if (!error) {
-      const character = JSON.parse(body);
-      console.log(character.name);
-      printCharacters(characters, index + 1);
-    }
-  });
-}

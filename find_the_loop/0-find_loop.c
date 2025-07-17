c25d73e66d5c5f755c7926e16590206792b3b590
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-17 21:46:34 +0100

    Update 0-find_loop.c

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the node where a loop starts in a linked list
- * @head: Pointer to the head of the linked list
+ * find_listint_loop - finds the first node of a loop in a singly linked list
+ * @head: pointer to the head of the linked list
  *
- * Return: Address of the node where the loop starts, or NULL if no loop
+ * Return: address of node where loop starts, or NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *turtle = head;
-	listint_t *hare = head;
+	listint_t *turtle = head, *hare = head;
 
 	if (head == NULL || head->next == NULL)
 		return (NULL);
 
-	while (hare != NULL && hare->next != NULL)
+	while (hare->next && hare->next->next)
 	{
 		turtle = turtle->next;
 		hare = hare->next->next;
@@ -30,6 +29,5 @@ listint_t *find_listint_loop(listint_t *head)
 			return (turtle);
 		}
 	}
-
 	return (NULL);
 }

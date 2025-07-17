fe8100a5204f5d72dc9349e26524f0bf50946e59
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-17 21:53:14 +0100

    Delete merge_sort directory

--- merge_sort/sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-#include <stddef.h>
-void merge(int *array, int *buffer, size_t left, size_t mid, size_t right)
-void merge_sort_recursive(int *array, int *buffer, size_t left, size_t right)
-void merge_sort(int *array, size_t size)
-#endif

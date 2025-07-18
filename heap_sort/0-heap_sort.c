97097d5c3529e6177f0d0d7f3eabd19c9c5b38f7
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-18 16:54:12 +0100

    Update 0-heap_sort.c

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,67 +1,80 @@
 #include "sort.h"
 
 /**
- * swap - Swap two integers in an array
- * @a: First integer
- * @b: Second integer
- */
-void swap(int *a, int *b)
-{
-	if (*a != *b)
-	{
-		int tmp = *a;
-		*a = *b;
-		*b = tmp;
-	}
-}
-
-/**
- * heapify - Maintains the max-heap property for a subtree
- * @array: The array to sort
- * @size: Total size of the array
- * @i: Index of the root of the subtree
- * @heap_size: Size of the heap
- */
-void heapify(int *array, size_t size, size_t i, size_t heap_size)
-{
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
-
-	if (left < heap_size && array[left] > array[largest])
-		largest = left;
-	if (right < heap_size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != i)
-	{
-		swap(&array[i], &array[largest]);
-		print_array(array, size);
-		heapify(array, size, largest, heap_size);
-	}
-}
-
-/**
- * heap_sort - Sorts an array using the Heap sort algorithm
- * @array: The array to sort
- * @size: Size of the array
+ * heap_sort - sorts an array of ints in ascending order
+ * using Heap Sort algorithm
+ *
+ * @array: the array of ints to sort
+ * @size: the size of the array to sort
+ *
  */
 void heap_sort(int *array, size_t size)
 {
-	ssize_t i;
-
-	if (!array || size < 2)
+	if (array == NULL || size <= 1)
 		return;
+	heap_sort_print(array, size, array, size);
+}
 
-	/* Build max heap */
-	for (i = (size / 2) - 1; i >= 0; i--)
-		heapify(array, size, i, size);
+/**
+ * heap_sort_print - heap sort with full array as parameter for printing
+ *
+ * @array: array to sort
+ * @size: size of array to sort
+ * @array_p: array to print
+ * @size_p: size of array to print
+ *
+ */
+void heap_sort_print(int *array, size_t size, int *array_p, size_t size_p)
+{
+	int i = 0, temp = 0;
 
-	/* Extract elements from heap */
-	for (i = size - 1; i > 0; i--)
+	for (i = (size - 1) / 2; i >= 0; i--)
 	{
-		swap(&array[0], &array[i]);
-		print_array(array, size);
-		heapify(array, size, 0, i);
+		heapify(array, size, i, array_p, size_p);
+	}
+	for (i = (size - 1); i > 0; i--)
+	{
+		temp = array[i];
+		array[i] = array[0];
+		array[0] = temp;
+		array_p[i] = array[i];
+		array_p[0] = array[0];
+		print_array(array_p, size_p);
+
+		heapify(array, i, 0, array_p, size_p);
+	}
+}
+
+/**
+ * heapify - reorders array like nodes swapping in a heap
+ *
+ * @array: the array of ints to sort
+ * @size: the size of the array to sort
+ * @i: current index in array
+ * @array_p: array to print
+ * @size_p: size of array to print
+ *
+ */
+void heapify(int *array, size_t size, int i, int *array_p, size_t size_p)
+{
+	int max = i;
+	int left_child = 2 * i + 1;
+	int right_child = 2 * i + 2;
+	int temp = 0;
+
+	if (left_child < (int)size && array[left_child] > array[max])
+		max = left_child;
+	if (right_child < (int)size && array[right_child] > array[max])
+		max = right_child;
+	if (max != i)
+	{
+		temp = array[i];
+		array[i] = array[max];
+		array[max] = temp;
+		array_p[i] = array[i];
+		array_p[max] = array[max];
+		print_array(array_p, size_p);
+
+		heapify(array, size, max, array_p, size_p);
 	}
 }

91d13586b07c2c9dc9c4b5ea80cda6b2d494f55d
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-21 11:56:30 +0100

    Update 0-wildcmp.c

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,34 +1,25 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings, allowing wildcard '*'
- * @s1: the first string
- * @s2: the second string, may contain '*'
+ * wildcmp - Compares two strings considering the special character *
+ * @s1: The first string to compare
+ * @s2: The second string to compare, which may contain the special character *
  *
- * Return: 1 if strings match, otherwise 0
+ * Return: 1 if the strings can be considered identical, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s2 == '*')
-	{
-		while (*s2 == '*')
-			s2++;
-		if (*s2 == '\0')
-			return (1);
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2))
-				return (1);
-			s1++;
-		}
-		return (0);
-	}
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	return (0);
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '\0')
+return (1);
+if (*s1 == '\0')
+return (wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+return (0);
 }

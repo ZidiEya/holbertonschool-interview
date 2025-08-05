04fd28b6a73e1bbcb56a91a35f0de5fc51c72e3e
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-05 10:55:59 +0100

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,33 +1,30 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters, 0);
-});
-
-function printCharacters(characters, index) {
-  if (index >= characters.length) return;
-
-  request(characters[index], (err, res, body) => {
-    if (!err) {
-      const character = JSON.parse(body);
-      console.log(character.name);
-      printCharacters(characters, index + 1);
-    }
+const asyncGetChar = async function (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(err);
+      }
+    });
   });
-}
+};
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  function (err, res, body) {
+    if (err) { console.log(err); }
+    const jsn = JSON.parse(body);
+    const chars = jsn.characters;
+    (async function () {
+      for (let i = 0; i < chars.length; i++) {
+        const res = await asyncGetChar(chars[i]);
+        const name = JSON.parse(res);
+        console.log(name.name);
+      }
+    })();
+  }
+);

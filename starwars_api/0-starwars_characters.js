ae7b220605f7cbc2021190ffe60cbfd82d8ae1ac
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-08-05 10:51:15 +0100

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,25 +1,33 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
-        });
-      });
-      console.log(res);
-    }
+    console.error(error);
+    return;
   }
+
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters, 0);
 });
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) return;
+
+  request(characters[index], (err, res, body) => {
+    if (!err) {
+      const character = JSON.parse(body);
+      console.log(character.name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
